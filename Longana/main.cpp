25bb84c324d9034430f94173731b886f1312756e
Bishal Regmi <bregmi1@ramapo.edu>
2017-09-11 12:22:41 -0400

    Initial Commit

---
+++ Longana/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Longana
+//
+//  Created by Bishal Regmi on 9/11/17.
+//  Copyright © 2017 Bishal Regmi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
